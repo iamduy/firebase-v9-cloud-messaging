cf149cf07d071be2696e670bde6470978609ffed
admin <duydk@its-global.vn>
2023-04-14 14:15:31 +0700

    update

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,7 +6,16 @@ self.addEventListener("fetch", () => {
   self.firebaseConfig = Object.fromEntries(urlParams);
 });
 
-firebase.initializeApp(self.firebaseConfig);
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOhLD9SzpRjPZIyrqFxRuJH7fHYJ2B2V0",
+  authDomain: 'sanmario2001.firebaseapp.com',
+  projectId: 'sanmario2001',
+  storageBucket: 'sanmario2001.appspot.com',
+  messagingSenderId: '854713213833',
+  appId: '1:854713213833:web:613d61dd797514d6bef758',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 if (firebase.messaging.isSupported()) {
   const messaging = firebase.messaging();
